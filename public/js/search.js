afa6996857c6b40c8434b3b4b7dc21fa479fd777
raulo2032 <raulaggio@gmail.com>
2022-08-15 11:51:57 -0300

    completo carga de proyecto

---
+++ public/js/search.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", () => {
+
+  const btn = document.querySelector(".btn");
+  btn.addEventListener("click", () => {
+    document.querySelector(".search").classList.toggle("show");
+  });
+
+  document.querySelector(".burger-menu").addEventListener("touchstart", () => {
+    document.querySelector(".nav-burger").classList.toggle("show-burger")
+  })
+
+  document.querySelector(".btn").addEventListener("touch", () => {
+    document.querySelector(".search").classList.toggle("show");
+  });
+
+  /*   document.querySelector(".burger-menu").addEventListener("touch", ()=>{
+      document.querySelector(".nav-burger").classList.toggle("show-burger")
+    }) */
+
+})
